c626b42a93710079687e4c1ac2e621922417fe0e
YesmeAlba <144072126+YesmeAlba@users.noreply.github.com>
2025-04-09 17:47:51 -0600

    Initial commit

---
+++ Assets/Scripts/secondToDestroy.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+ 
+ 
+public class DestroySeconds :  MonoBehaviour
+
+{
+
+    [SerializeField]
+
+    private float secondsToDestroy = 5f;
+
+    private void Start()
+
+    {
+
+        Destroy(gameObject, secondsToDestroy);
+
+    }
+ 
+}
+
+ 
